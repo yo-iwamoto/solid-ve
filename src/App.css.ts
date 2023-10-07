3bcaed6d06b74dddd51aaf39f9d76216a5b28347
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-10-07 12:09:21 +0900

    use signal for dynamic view

--- src/App.css.ts
+++ src/App.css.ts
@@ -1,15 +1,59 @@
-import { style } from '@vanilla-extract/css';
-import { fontSize, fontWeight } from './styles/theme.css';
+import { style, keyframes, styleVariants } from '@vanilla-extract/css';
+import { colors, fontSize, fontWeight } from './styles/theme.css';
 
-export const styles = {
-  root: style({
-    display: 'grid',
-    placeItems: 'center',
-    minHeight: '100vh',
-  }),
-  heading: style({
-    fontSize: fontSize.xxxl,
-    fontStyle: 'italic',
-    fontWeight: fontWeight.bold,
-  }),
-};
+export const root = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 100,
+  justifyContent: 'center',
+  alignItems: 'center',
+  minHeight: '100vh',
+});
+
+export const heading = style({
+  fontSize: fontSize.xxxl,
+  fontStyle: 'italic',
+  fontWeight: fontWeight.bold,
+  textAlign: 'center',
+});
+
+export const button = style({
+  border: 0,
+  paddingInline: 20,
+  paddingBlock: 10,
+  backgroundColor: colors.brand,
+  color: colors.white,
+  fontWeight: fontWeight.bold,
+  fontSize: fontSize.xl,
+  borderRadius: 4,
+  cursor: 'pointer',
+  transitionProperty: 'opacity',
+  transitionDuration: '0.1s',
+  ':hover': {
+    opacity: 0.8,
+  },
+});
+
+const iconBase = style({
+  height: 320,
+  width: 320,
+});
+
+const spin = keyframes({
+  '0%': {
+    transform: 'rotate(0deg)',
+  },
+  '100%': {
+    transform: 'rotate(360deg)',
+  },
+});
+
+export const icon = styleVariants({
+  default: [iconBase],
+  spin: [
+    iconBase,
+    {
+      animation: `${spin} 5s linear infinite`,
+    },
+  ],
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { styles } from './App.css';
+import { SolidIcon } from './components/SolidIcon';
+import * as styles from './App.css';
+import { createSignal } from 'solid-js';
 
 export function App() {
+  const [isSpining, setIsSpining] = createSignal(true);
+
+  function toggle() {
+    setIsSpining((prev) => !prev);
+  }
+
   return (
     <main class={styles.root}>
       <h1 class={styles.heading}>Hello Solid.js🍦</h1>
+
+      <button type='button' onClick={toggle} class={styles.button}>
+        {isSpining() ? 'Stop' : 'Start'}
+      </button>
+
+      <SolidIcon class={isSpining() ? styles.icon.spin : styles.icon.default} />
     </main>
   );
 }
